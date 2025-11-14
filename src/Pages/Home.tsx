316d471aeee44ab4764e7343020933f15e73e8e6
m.ali <majid.live@gmail.com>
2025-11-14 18:57:45 +0330

    v2

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import Header from "../component/Header";
+import Hero from "../component/Hero";
 
 const Home = () => {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Hero />
+      </div>
+    </>
   );
 };
 
---
+++ src/component/Hero.tsx
@@ -0,0 +1,163 @@
+import React from "react";
+import heroPic from "../img/75.jpg";
+import { motion } from "motion/react";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col justify-center items-center bg-white backdrop-blur-3xl backdrop-green pt-25">
+      <div className="flex flex-row justify-between gap-7">
+        <div className="flex fle-row  pt-10 ">
+          <img
+            className="w-10 h-10 rounded-full -mr-4 outline-white border-2  outline-2"
+            src={heroPic}
+            alt=""
+          />
+          <motion.img
+            initial={{
+              top: 0,
+            }}
+            transition={{
+              duration: 2,
+            }}
+            whileInView={{
+              top: 4,
+            }}
+            viewport={{
+              once: true,
+            }}
+            className="w-10 h-10 rounded-full -mr-4"
+            src={heroPic}
+            alt=""
+          ></motion.img>
+
+          <img className="w-10 h-10 rounded-full -mr-4" src={heroPic} alt="" />
+
+          <img className="w-10 h-10 rounded-full -mr-4" src={heroPic} alt="" />
+
+          <img className="w-10 h-10 rounded-full mr-4" src={heroPic} alt="" />
+
+          <div className="flex flex-col gap-1">
+            <div className="flex flex-row">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-star text-transparent fill-green-600"
+                aria-hidden="true"
+              >
+                <path d="M11.525 2.295a.53.53 0 0 1 .95 0l2.31 4.679a2.123 2.123 0 0 0 1.595 1.16l5.166.756a.53.53 0 0 1 .294.904l-3.736 3.638a2.123 2.123 0 0 0-.611 1.878l.882 5.14a.53.53 0 0 1-.771.56l-4.618-2.428a2.122 2.122 0 0 0-1.973 0L6.396 21.01a.53.53 0 0 1-.77-.56l.881-5.139a2.122 2.122 0 0 0-.611-1.879L2.16 9.795a.53.53 0 0 1 .294-.906l5.165-.755a2.122 2.122 0 0 0 1.597-1.16z"></path>
+              </svg>
+            </div>
+            <div>
+              <p>Used by 10,000+ users</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col justify-center items-center pt-12">
+        <h2 className="text-[60px] font-bold wrap-break-word w-[60%] text-center">
+          Land your dream job with AI-powered resumes.
+        </h2>
+        <p className="wrap-break-word w-[29%] text-center pt-4">
+          Create, edit and download professional resumes with AI-powered
+          assistance.
+        </p>
+      </div>
+      <div className="flex flex-row gap-2 pt-4">
+        <div className="flex flex-row gap-2 pt-3 px-5">
+          <button className="bg-green-600 outline-white border-2 border-green-500 px-9  text-white tracking-wider rounded-full text-[14px] py-3 ">
+            Get Started
+          </button>
+          <button className=" flex flex-row items-center text-[14px] border-2 px-4 rounded-full border-gray-400 text-gray-500 gap-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="1"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              class="lucide lucide-video size-5"
+              aria-hidden="true"
+              data-metatip="true"
+            >
+              <path d="m16 13 5.223 3.482a.5.5 0 0 0 .777-.416V7.87a.5.5 0 0 0-.752-.432L16 10.5"></path>
+              <rect x="2" y="6" width="14" height="12" rx="2"></rect>
+            </svg>
+            <span>Tray Demo</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
